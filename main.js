b90c8fda647c93fb6d33a126a1a4ac79543c2ff6
stephen <sdelius.prsvr@gmail.com>
2023-09-21 16:07:33 -0500

    homework completed, passes tests

--- main.js
+++ main.js
@@ -2,9 +2,61 @@
  * YOUR CODE BELOW! *
  ********************/
 
+function moreThan5(num) {
+  return num > 5
+}
 
+function isNewTopScore(newScore, topScore) {
+  return newScore > topScore
+}
 
+function isInDanger(grade) {
+  return grade >= 60 && grade <= 71
+}
 
+function isCoasting(grade) {
+  return grade >= 72 && grade <= 83
+}
+
+function isSucceeding(grade) {
+  return grade >= 84 && grade <= 92
+}
+
+function isFailing(grade) {
+  return grade < 60
+}
+
+function isAcing(grade) {
+  return grade > 92
+}
+
+function isStudent(role) {
+  return role === 'student'
+}
+
+function isTeacher(role) {
+  return role === 'teacher'
+}
+
+function isAdmin(role) {
+  return role === 'admin'
+}
+
+function isElementary(level) {
+  return level === 'elementary'
+}
+
+function areDifferentPeople(name1, name2) {
+  return name1 !== name2
+}
+
+function isMiddleSchoolTeacher(role, level) {
+  return role === 'teacher' && level >= 6 && level <= 8
+}
+
+function notAnElementarySchoolAdministrator(level, role) {
+  return level !== 'elementary' || role !== 'admin'
+}
 
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
